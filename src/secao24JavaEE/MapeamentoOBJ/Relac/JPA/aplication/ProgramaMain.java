01b54a6bd64bb2e092b5f30f7a071471ad62490a
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2020-02-17 08:56:02 -0300

    Nivelamento JPA / Hibernate - PARTE 2

--- src/secao24JavaEE/MapeamentoOBJ/Relac/JPA/aplication/ProgramaMain.java
+++ src/secao24JavaEE/MapeamentoOBJ/Relac/JPA/aplication/ProgramaMain.java
@@ -1,20 +1,39 @@
 package secao24JavaEE.MapeamentoOBJ.Relac.JPA.aplication;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import secao24JavaEE.MapeamentoOBJ.Relac.JPA.dominio.Pessoa;
 
 public class ProgramaMain {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(1, "Jorge Ferraz", "jferraz@cpqd.com.br");
-		Pessoa p2 = new Pessoa(1, "Aline Ferraz", "aferraz@cpqd.com.br");
-		Pessoa p3 = new Pessoa(1, "Heloa Ferraz", "hferraz@cpqd.com.br");
+		/*Pessoa pj = new Pessoa(null, "Jorge Ferraz", "jferraz@cpqd.com.br");
+		Pessoa pa = new Pessoa(null, "Aline Ferraz", "aferraz@cpqd.com.br");
+		Pessoa ph = new Pessoa(null, "Heloa Ferraz", "hferraz@cpqd.com.br");*/
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
 		
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);		
+		Pessoa p = em.find(Pessoa.class, 6);
 		
+		
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		
+		/*em.getTransaction().begin();
+		em.persist(pj);
+		em.persist(pa);
+		em.persist(ph);
+		em.getTransaction().commit();*/
+		
+		System.out.println("Pronto");
+		em.close();
+		emf.close();
 	}
 
 }
